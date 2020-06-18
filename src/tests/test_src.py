e54e93143377a3afacb3ef11a742a26c3cb4eb3a
Arthur Imbert <arthur.imbert.pro@gmail.com>
2020-06-18 17:39:30 +0200

    remove test

--- src/tests/test_src.py
+++ src/tests/test_src.py
@@ -10,13 +10,3 @@ import pytest
 from src.mymodule import myfunction
 
 
-@pytest.mark.parametrize("value", [0, 1, 2, 3, 4, 5, 0., 1., 2., 3., 4., 5.])
-def test_myfunction(value):
-    # get value
-    xx = myfunction(value)
-
-    # check value
-    if isinstance(value, int):
-        assert xx == value * value
-    else:
-        assert xx is None
